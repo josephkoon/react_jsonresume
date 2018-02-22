1a6dce8ee191af049214e5a070ff424b1b02bc8a
Joseph Koon <josephandrewkoon@gmail.com>
2018-02-21 22:57:27 -0800

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+
+
+			<footer>
+
+			      <div className="row">
+
+			         <div className="twelve columns">
+
+			            <ul className="copyright">
+			               <li>&copy; Copyright 2014 CeeVee</li>
+			               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>   
+			            </ul>
+
+			         </div>
+
+			         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+
+			      </div>
+
+			   </footer> 
+
+
+      </div>
+    );
+  }
+}
+
+export default Footer;
